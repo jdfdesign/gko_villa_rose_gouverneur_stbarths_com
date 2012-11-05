6baeded19bf2982ffbb23367f3e5617cbe216a6e
jdfdesign <regis@joufdesign.com>
2012-11-05 03:54:09 -0400

    bump

--- db/migrate/20121026135552_clean_core_elements.gko_core.rb
+++ db/migrate/20121026135552_clean_core_elements.gko_core.rb
@@ -1,16 +1,16 @@
 # This migration comes from gko_core (originally 20120825130500)
 class CleanCoreElements < ActiveRecord::Migration
+  class Section < ActiveRecord::Base
+  
+  end
+  
   def up
       drop_table :element_texts if table_exists?(:element_texts)
       drop_table :element_assignments if table_exists?(:element_assignments)
       drop_table :element_text_translations if table_exists?(:element_text_translations)
-      
-      
-      class Section < ActiveRecord::Base
-      end
+
       Section.reset_column_information 
-      
-      
+
       pref_keys = ['column_count', 'default_order', 'listing_description_length', 'listing_omission', 'children_thumb_size']
       connection.execute("UPDATE sections SET type='Portfolio' WHERE type='GalleryList'")
       connection.execute("UPDATE contents SET type='Project' WHERE type='Gallery'")

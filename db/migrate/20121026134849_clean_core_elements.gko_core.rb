30f9ff6e26e7c7bc7be9e0d6fb74040c70fb59d4
Regis Bruggheman <regis@joufdesign.com>
2014-04-03 22:29:06 +0200

    Merge branch 'master' of https://github.com/jdfdesign/gko_villa_rose_gouverneur_stbarths_com
    Conflicts:
    	Gemfile
    	Gemfile.lock
    	app/views/application/_header.html.erb
    	app/views/pages/_page.html.erb
    	app/views/pages/contact.html.erb
    	app/views/pages/home.html.erb
    	app/views/pages/one_page.html.erb

--- db/migrate/20121026134849_clean_core_elements.gko_core.rb
+++ db/migrate/20121026134849_clean_core_elements.gko_core.rb
@@ -1,17 +1,12 @@
 # This migration comes from gko_core (originally 20120825130500)
 class CleanCoreElements < ActiveRecord::Migration
-
   def up
       drop_table :element_texts if table_exists?(:element_texts)
       drop_table :element_assignments if table_exists?(:element_assignments)
       drop_table :element_text_translations if table_exists?(:element_text_translations)
-
-
-
       pref_keys = ['column_count', 'default_order', 'listing_description_length', 'listing_omission', 'children_thumb_size']
       connection.execute("UPDATE sections SET type='Portfolio' WHERE type='GalleryList'")
       connection.execute("UPDATE contents SET type='Project' WHERE type='Gallery'")
-
       Site.all.each do |site|
         Site.current = site
         site.sections.all.each do |m|
